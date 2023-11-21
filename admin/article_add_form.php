47c1ce2c519358274e73e9ea6eb130ebe82738c3
fabien <flierville@cesi.fr>
2023-11-21 09:22:21 +0100

    Exercice  $_POST

--- admin/article_add_form.php
+++ admin/article_add_form.php
@@ -1,1 +1,18 @@
-<?php
+<?php require ("../inc/config.php"); ?>
+<?php require ("../inc/header.php"); ?>
+<h1>Ajouter u article - Partie Admin</h1>
+<form action="article_show.php" method="post">
+    <input type="text" name="titre">
+    <textarea name="description"></textarea>
+    <input type="date" name="datepublication">
+    <select name="auteur">
+        <option value="Samuel">Samuel</option>
+        <option value="Killian">Killian</option>
+        <option value="Noé">Noé</option>
+        <option value="Dylan">Dylan</option>
+        <option value="Thomas B">Thomas B</option>
+    </select>
+    <input type="submit">
+</form>
+
+<?php require ("../inc/footer.php"); ?>
