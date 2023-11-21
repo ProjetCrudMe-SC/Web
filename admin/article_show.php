85d00becb1b58867e2d903be109994d40005b90e
fabien <flierville@cesi.fr>
2023-11-21 09:15:07 +0100

    Exercice  $_POST

---
+++ admin/article_show.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Créer /admin/article_add_form.php puis faire un formulaire pour « Ajouter un article » dont voici les champs nécessaires :
+-	Titre : input type text
+-	Description : texarea
+-	Date de publication : input type date
+-	Auteur : un <select> avec 5 <option> que sont les prénoms de camarades de classes
+Attention, pensez à bien mettre un attribut « name » à vos éléments de formulaire
+Pensez aux require pour intégrer le footer et header html.
+
+Créer /admin/article_show.php : récupérez les données du formulaire et afficher l’article dans du HTML. Pensez aux require pour intégrer le footer et header html.
+
+*/
