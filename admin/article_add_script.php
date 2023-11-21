a4e445cb8f160d0aee14dd2df75dba41a0a5eb79
fabien <flierville@cesi.fr>
2023-11-21 13:55:31 +0100

    Modif article

--- admin/article_add_script.php
+++ admin/article_add_script.php
@@ -4,10 +4,9 @@ require ("../inc/config.php");
 $requete = $bdd->prepare("INSERT INTO articles (Titre, Description, DatePublication, Auteur) VALUES(:Titre, :Description, :DatePublication, :Auteur)");
 
 $execute = $requete->execute([
-    "Titre" => "Troisième",
-    "Description" => "Mon troisième article",
-    "DatePublication" => "2023-11-11",
-    "Auteur" => "Fabien",
+"Titre" => $_POST["titre"],
+    "Description" => $_POST["description"],
+    "DatePublication" => $_POST["datepublication"],
+    "Auteur" => $_POST["auteur"],
 ]);
-// Tester en allant sur www.cesi.local/admin/article_add_script.php
-var_dump($execute);
+header("Location: /admin");
