fd2baabe8230ec979b022cf44854e0da8a1b6d05
fabien <flierville@cesi.fr>
2023-11-21 11:33:16 +0100

    Exercice  BDD

--- inc/config.php
+++ inc/config.php
@@ -1,11 +1,21 @@
 <?php
-/*
- * Dans le répertoire « inc » créer le fichier « config.php ». Dans ce fichier créer une fonction qui retourne les N premiers mots d’un paragraphe de texte (dans le cadre d’un blog = un extrait de l’article)
-Ensuite intégrez ce fichier config.php dans /index.php dès la première ligne (ce fichier sera prioritaire sur le reste des opérations
-Appelez cette fonction dans un paragraphe sur le fichier index.php (mettez une très grande phrase)
-Aide : Sur Google tapez « php get first x words of string »
+/* Database Connexion */
+const _DBHOSTNAME_ = "cours_php-mariadb106";
+const _DBUSERNAME_ = "docker";
+const _DBPASSWORD_ = "cesi";
+const _DBNAME_ = "docker";
+const _DBPORT_ = 3306;
 
- */
+try{
+    $bdd = new PDO(
+        dsn: "mysql:host="._DBHOSTNAME_.";port="._DBPORT_.";dbname="._DBNAME_.";charset=utf8",
+        username: _DBUSERNAME_,
+        password: _DBPASSWORD_
+    );
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch (Exception $e){
+    die("Erreur : {$e->getMessage()}");
+}
 
 function premiersMots($phrase, $n) {
     preg_match('/^(\S+\s+){0,' . ($n - 1) . '}\S+/', $phrase, $matches);
--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php require ("./inc/config.php"); ?>
-<?php require ("./inc/header.php"); ?>
 <?php
-var_dump($_GET);
+if(isset($_POST["search"])){
+    $requete = $bdd->query("SELECT * FROM articles WHERE Id={$_POST["search"]}");
+    $articles = $requete->fetch(PDO::FETCH_ASSOC);
+}else{
+    $requete = $bdd->query("SELECT * FROM articles");
+    $articles = $requete->fetchAll(PDO::FETCH_ASSOC);
+}
+var_dump($articles);
 ?>
+<?php require ("./inc/header.php"); ?>
+
 <h1>Bienvenue sur notre Blog</h1>
 <?php
 $variableduchampinvisible = "123";
-var_dump($_POST);
 ?>
 <form name="recherche" method="post">
     <input placeholder="ID Sql" name="search" type="text">
     <input type="hidden" name="champInvisible" value="<?php echo $variableduchampinvisible; ?>">
 </form>
 
-<form name="recherche" method="post" action="submitform.php">
-    <input type="text" name="prenom">
-    <input type="text" name="nom">
-    <input type="date" name="date">
-    <input type="submit">
-</form>
+
 
 <?php require ("./inc/footer.php"); ?>
