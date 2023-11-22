152287712ad2fddea58b4e6a5699a6d506463c10
fabien <flierville@cesi.fr>
2023-11-22 11:08:35 +0100

    upload file

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php require ("./inc/config.php"); ?>
 <?php
+require ("./objet/Article.php");
+use objet\Article;
+?>
+<?php
 if(isset($_POST["search"])){
     $requete = $bdd->prepare("SELECT * FROM articles WHERE Id = :IDARTICLE OR Titre like :TITREARTICLE");
     $requete->execute([
@@ -11,7 +15,7 @@ if(isset($_POST["search"])){
     $requete = $bdd->query("SELECT * FROM articles");
     $articles = $requete->fetchAll(PDO::FETCH_ASSOC);
 }
-var_dump($articles);
+
 ?>
 <?php require ("./inc/header.php"); ?>
 
@@ -23,6 +27,30 @@ $variableduchampinvisible = "123";
     <input placeholder="ID Sql" name="search" type="text">
     <input type="hidden" name="champInvisible" value="<?php echo $variableduchampinvisible; ?>">
 </form>
+
+<?php
+$article = new Article();
+$article->setTitre("Mon titre");
+$article->setDescription("dsjiofsd lfshuqldsqluhsqjkhfsqkjlh   hefh qulfqlf hdkqsh jk u fd uksdqkjsdq kljfs lkj lklk  jkhf lkq ljkdq leq kq kjk kfe kulfd");
+var_dump($article);
+$article2 = new Article();
+$article2->setTitre("Mon titre2");
+var_dump($article2);
+$article3 = $article;
+$article3->setTitre("Mon titre 3");
+var_dump($article->strlen());
+
+$article->setAuteur("Brice")->setDatePublication(new \DateTime());
+$sql = $article->SqlAdd($bdd);
+var_dump($sql);
+
+?>
+
+
+
+
+
+
 
 
 
