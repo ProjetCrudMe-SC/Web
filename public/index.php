6fa80858c1e1f4428fd2ec9c0f277a0d8a194be9
fabien <flierville@cesi.fr>
2023-11-22 11:47:52 +0100

    Passage en MVC (intégration du site classic pour copier/coller)

--- public/index.php
+++ public/index.php
@@ -1,2 +1,20 @@
 <?php
-echo ("Bienvenue sur notre MVC");
+function chargerClasse($classe){
+    //$classe va ressembler à src\Model\Article
+    $ds = DIRECTORY_SEPARATOR;
+    // JE veux le document ROOT (c'est à dire www)
+    $dir = __DIR__."$ds.."; // Je remonte d'un cran pour wwww
+
+    //Remplacer les séparateur du namespace "src\Model\Article"
+    $className = str_replace('\\', $ds, $classe);
+
+    $file = "{$dir}{$ds}{$className}.php";
+    if(is_readable($file)){
+        require_once $file;
+    }
+}
+spl_autoload_register("chargerClasse");
+
+$article = new \src\Model\Article();
+$article->setTitre("Coucou")->setDescription("Coucou");
+var_dump($article);
