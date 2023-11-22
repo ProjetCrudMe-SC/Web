da4425877081689b746002add1d158ea83bc92df
fabien <flierville@cesi.fr>
2023-11-22 15:06:39 +0100

    routeur

--- public/index.php
+++ public/index.php
@@ -17,9 +17,27 @@ spl_autoload_register("chargerClasse");
 
 
 
+$controller = (isset($_GET['controller'])) ? $_GET['controller'] : '';
+$action = (isset($_GET['action'])) ? $_GET['action'] : '';
+$param = (isset($_GET['param'])) ? $_GET['param'] : '';
 
-$controller = new src\Controller\ArticleController();
-echo $controller->fixtures();
-
+if($controller != ''){
+    try {
+        $class = "src\Controller\\".$controller."Controller";
+        if (class_exists($class)) {
+            $controller = new $class();
+            if (method_exists($class, $action)) {
+                echo $controller->$action($param);
+            }else { echo'Acion n\'existe pas pour cette action !';}
+        }else { echo 'Le controlleur n\'existe pas pour cette action !';}
+    }
+    catch(Exception $e) {
+        // Penser à Gérer l’exception
+    }
+}else {
+    //Route par défaut (/)
+    $controller = new \src\Controller\ArticleController();
+    echo $controller->index();
+}
 
 
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,12 +15,6 @@ class ArticleController{
     }
 
     public function fixtures(): string{
-        //copier coller le contenu de fixtures.php
-        // et l'adapter ici (connexion à la BDD)
-        // Puis l'appeler depuis le index.php
-
-        // Pour ajouter un article pensez à utiliser
-        // la méthode SqlAdd (du model Article)
         Article::SqlFixtures();
         return "<p>Fixtures ok </p>";
     }
