267efda2aa165f4b935e612e1bea22543196a9e8
fabien <flierville@cesi.fr>
2023-11-23 10:17:32 +0100

    TWIG Add Article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -29,4 +29,47 @@ class ArticleController extends AbstractController {
         Article::SqlDelete($id);
         header("Location: /?controller=Article&action=all");
     }
+
+    public function add(){
+        if(isset($_POST["Titre"]) && isset($_POST["Description"])){
+            $sqlRepository = null;
+            $nomImage = null;
+
+            if(!empty($_FILES["Image"]["name"])){
+                $tabExt = ["jpg", "jpeg", "gif", "png"]; // Extension autorisée
+                $extension = pathinfo($_FILES["Image"]["name"], PATHINFO_EXTENSION);
+                if(in_array(strtolower($extension), $tabExt)){
+                    //Fabriquer le répertoire d'auccil façon Wrodpress (YYYY/MM)
+                    $dateNow = new \DateTime();
+                    $repository = "./uploads/images/{$dateNow->format("Y/m")}";
+                    if(!is_dir($repository)) {
+                        mkdir($repository, 0777, true);
+                    }
+                    $sqlRepository = $dateNow->format("Y/m");
+                    //Renommer le fichier image à la volée
+                    $nomImage = md5(uniqid()).".".$extension;
+
+                    //Upload du fichier
+                    move_uploaded_file($_FILES["Image"]["tmp_name"], $repository."/".$nomImage);
+                }
+            }
+
+           $article = new Article();
+            $article->setTitre($_POST["Titre"])
+                ->setDescription($_POST["Description"])
+                ->setDatePublication(new \DateTime($_POST["DatePublication"]))
+                ->setAuteur($_POST["Auteur"])
+                ->setImageRepository($sqlRepository)
+                ->setImageFileName($nomImage);
+            $result = $article->SqlAdd();
+
+
+            header("Location: /?controller=Article&action=all");
+        }
+        return $this->getTwig()->render("Article/add.html.twig");
+    }
+
+
+
+
 }
