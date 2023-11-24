b83b287dfd28502c356e186d39d1fbe1425c646d
Fabien LIERVILLE <fabien.lierville27@gmail.com>
2023-11-24 11:27:33 +0100

    Change script SQL Create Table

--- src/Service/JwtService.php
+++ src/Service/JwtService.php
@@ -6,7 +6,7 @@ use Firebase\JWT\JWT;
 class JwtService{
     public static String $secretKey = "cesiblog";
 
-    public static function createToken(array $datas) : JWT {
+    public static function createToken(array $datas) : String {
         $issuedAt = new \DateTime(); //Date de publication
         $expire = new \DateTime();
         $expire->modify('+6 minutes');
