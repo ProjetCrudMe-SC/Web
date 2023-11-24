04fae23a50b5ccc7fb44f7be8cb6502f1bd62301
Fabien LIERVILLE <fabien.lierville27@gmail.com>
2023-11-24 11:16:39 +0100

    Change script SQL Create Table

---
+++ src/Service/JwtService.php
@@ -0,0 +1,28 @@
+<?php
+namespace src\Service;
+
+use Firebase\JWT\JWT;
+
+class JwtService{
+    public static String $secretKey = "cesiblog";
+
+    public static function createToken(array $datas) : JWT {
+        $issuedAt = new \DateTime(); //Date de publication
+        $expire = new \DateTime();
+        $expire->modify('+6 minutes');
+
+        $data = [
+          "iat" => $issuedAt->getTimestamp(), // Date création
+            "iss" => "cesi.local",
+            "nbf" => $issuedAt->getTimestamp(), //Utilisable pas avant ...
+            "exp" => $expire->getTimestamp(),
+            "datas" => $datas
+        ];
+
+        $jwt = JWT::encode($data, self::$secretKey, "HS512");
+
+        return $jwt;
+
+    }
+
+}
