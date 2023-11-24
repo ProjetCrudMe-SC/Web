4d6941a9faf1179dd35c8ea0c462ef4f9fb6a3b9
Fabien LIERVILLE <fabien.lierville27@gmail.com>
2023-11-24 09:20:29 +0100

    Change script SQL Create Table

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,9 +1,22 @@
 <?php
 namespace src\Controller;
 
+use src\Model\User;
+
 class UserController extends AbstractController {
 
     public function create(){
+        if(isset($_POST["nomprenom"]) && isset($_POST["mail"]) && isset($_POST["password"]) && isset($_POST["roles"])){
+            $user = new User();
+            $hashpass = password_hash($_POST["password"], PASSWORD_BCRYPT, ["cost"=>12]);
+            $user->setNomPrenom($_POST["nomprenom"])
+                ->setMail($_POST["mail"])
+                ->setPassword($hashpass)
+                ->setRoles($_POST["roles"]);
+            $result = User::SqlAdd($user);
+
+            header("location:/");
+        }
         return $this->getTwig()->render("User/create.html.twig");
     }
 
